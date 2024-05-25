e5285ff748b3c6259da1c57efd374a018a2151fa
Mykal Coleman <123437042+Mykaru@users.noreply.github.com>
2024-05-25 14:54:58 -0700

    Update

---
+++ IntroUnity/Assets/move_2D.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class move_2D : MonoBehaviour 
+{
+
+    public float acceleration = 4.0f;
+    public float jumpforce = 5.0f;
+    private bool isJumping = false;
+
+    private Rigidbody2D rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    bool isGrounded()
+    {
+        int layerMask = LayerMask.GetMask("Ground");
+        Vector2 offset = new Vector2(0.0f, 0.0f);
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        return Physics2D.Raycast(position + offset, Vector2.down, 
+            0.6f, layerMask);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            isJumping = true;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float mass = rb.mass;
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 movement = new Vector2(-acceleration * mass, 0.0f);
+            rb.AddForce(movement, ForceMode2D.Force);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 movemenet = new Vector2(acceleration * mass, 0.0f);
+            rb.AddForce(movemenet, ForceMode2D.Force);
+        }
+
+        if (isJumping && isGrounded())
+        {
+            rb.AddForce(new Vector2(0.0f, jumpforce), ForceMode2D.Impulse);
+            isJumping = false;
+        }
+        
+    }
+
+}
