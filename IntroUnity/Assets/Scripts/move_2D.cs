034a1fa614642eb7d5b9f8c22859bef445dd2427
Mykal Coleman <123437042+Mykaru@users.noreply.github.com>
2024-06-01 12:12:03 -0700

    Clamped the maximum speed

--- IntroUnity/Assets/Scripts/move_2D.cs
+++ IntroUnity/Assets/Scripts/move_2D.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 
 public class move_2D : MonoBehaviour 
 {
-
     public float acceleration = 4.0f;
     public float jumpforce = 5.0f;
+    public float maxSpeed = 5.0f; // Maximum speed for the character
     private bool isJumping = false;
-
     private Rigidbody2D rb = null;
 
     // Start is called before the first frame update
@@ -23,15 +22,13 @@ public class move_2D : MonoBehaviour
         int layerMask = LayerMask.GetMask("Ground");
         Vector2 offset = new Vector2(0.0f, 0.0f);
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
-        return Physics2D.Raycast(position + offset, Vector2.down, 
-            0.6f, layerMask);
+        return Physics2D.Raycast(position + offset, Vector2.down, 0.6f, layerMask);
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             isJumping = true;
         }
-        
     }
 
     // Update is called once per frame
@@ -45,16 +42,17 @@ public class move_2D : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            Vector2 movemenet = new Vector2(acceleration * mass, 0.0f);
-            rb.AddForce(movemenet, ForceMode2D.Force);
+            Vector2 movement = new Vector2(acceleration * mass, 0.0f);
+            rb.AddForce(movement, ForceMode2D.Force);
         }
+
+        // Clamp the velocity to the maximum speed
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y);
 
         if (isJumping && isGrounded())
         {
             rb.AddForce(new Vector2(0.0f, jumpforce), ForceMode2D.Impulse);
             isJumping = false;
         }
-        
     }
-
 }
